35221a601a68c903620ab8b4842092849eebe992
Alexandr Nurgaliev <orjda@mail.ru>
2024-04-10 17:15:41 +0300

    Initial commit

---
+++ number.py
@@ -0,0 +1,38 @@
+"""
+Создай класс `Number` c полем `value` (указывается при инициализации)
+
+Создай экземпляр, например `x = Number(7)`
+
+Добавь методы:
+
+`.get()` возвращает текущее value
+
+`.add(<значение>)` добавляет указанное число к value
+
+`.substract(<значение>)` вычитает указанное число из value
+"""
+
+class Number:
+
+    def __init__(self, value):
+        self.value = value
+
+    def get(self):
+        return self.value
+
+    def add(self, add_value):
+        self.add_value = add_value
+        self.value += add_value
+
+    def substract(self, minus_value):
+        self.minus_value = minus_value
+        self.value -= minus_value
+
+
+# код для проверки 
+n = Number(7)
+print(n.get())  # 7
+n.add(3)
+print(n.get())  # 10
+n.substract(5)
+print(n.get())  # 5
